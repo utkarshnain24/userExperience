578e507ba80c5e47abf900893f5c2570e3999c52
Utkarsh Nain <utkarsh.nain@xebia.com>
2024-09-08 04:20:18 +0530

    Initial Commit

---
+++ SwiftUIUserExp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIUserExp
+//
+//  Created by Utkarsh Nain on 08/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIUserExp/SwiftUIUserExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUserExpApp.swift
+//  SwiftUIUserExp
+//
+//  Created by Utkarsh Nain on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUserExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
